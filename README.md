aa0b5dfa3f545d7086c7183cfa334a9839b28fc5
Orlando Tomás <orlando.tomas@hotmail.com>
2025-01-02 23:34:57 +0100

    add new rules and assign them via cli flags

--- README.md
+++ README.md
@@ -2,30 +2,66 @@
 [![License](https://img.shields.io/github/license/orltom/on-call-schedule)](/LICENSE)
 
 # On-Call Schedule
-It helps to create or synchronize on-call schedules for team members, allowing users to define custom rules or use 
-predefined ones. Important factors, such as absences due to holidays or other time off, are automatically considered.
+This tool is a flexible command-line tool designed to simplify the creation and management of on-call schedules 
+for on-call teams. It addresses common limitations in existing incident management tools, such as lack of calendar 
+syncing, inadequate handling of holidays, and inflexible scheduling rules.
+
+# ✨ Current Features
+- Custom Rule-Based On-Call Scheduling: Create on-call schedules based on your own rules or use default rules, such as holiday checkers.
+- Multiple Export Formats: Generate on-call schedules in various formats, including CSV and JSON
+
+# 🛠️ Planned Features
+- iCalender Export
+- Automatically generate on-call schedules by syncing with a shared group calendar.
+- Export schedules directly to popular incident management tools.
+
+
+# Installation
+To install the latest version of ocsctl:
+```shell
+go install github.com/orltom/on-call-schedule@latest
+```
+
+Or clone the repository and build manually:
+
+```shell
+git clone https://github.com/orltom/on-call-schedule.git
+go build -o ocsctl ./cmd
+```
 
 ## Usage
+### Create on-call schedule plan
 Here is an example of how to create an on-call duty plan
 ```shell
-> cat > demo.json << EOL
+cat > team.json << EOL
 {
   "employees": [
     {"id": "joe@example.com", "name": "Joe"},
     {"id": "jan@example.com", "name": "Jan", "vacationDays": ["2024-01-06","2024-01-07"]},
-    {"id": "lee@example.com", "name": "Lee"}
+    {"id": "lee@example.com", "name": "Lee"},
+    {"id": "eva@example.com", "name": "Eva"}
   ]
 }
 EOL
 
-./ocsctl create \
+ocsctl create \
         --start "2024-01-01 00:00:00" \
         --end "2024-03-29 00:00:00" \
         --duration 168 \
-        --team-file demo.json \
+        --team-file team.json \
+        --primary-rules=vacation,minimumfourshiftgap
+        --secondary-rules=vacation,minimumtwoshiftgap
         --output table
 ```
+## Help Command
+```shell
+ocsctl [command] -h
+```
+
 
 ## Contributing
 Contributions are welcome in any form, be it code, logic, documentation, examples, requests, bug reports, ideas or
 anything else that will help this project move forward.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for more details.
--- cmd/create_command.go
+++ cmd/create_command.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/orltom/on-call-schedule/internal/cli"
@@ -14,22 +14,17 @@ import (
 	"github.com/orltom/on-call-schedule/pkg/apis"
 )
 
-var (
-	ErrMissingArguments = errors.New("missing required flag")
-	ErrInvalidArgument  = errors.New("invalid value")
-)
-
-type Converter int
+type Format int
 
 const (
-	CVS Converter = iota
+	CVS Format = iota
 	Table
 	JSON
 )
 
 func RunCreateShiftPlan(arguments []string) error {
-	enums := map[string]Converter{"CVS": CVS, "Table": Table, "json": JSON}
-	converters := map[Converter]func() apis.Exporter{
+	enums := map[string]Format{"CVS": CVS, "Table": Table, "json": JSON}
+	exporters := map[Format]func() apis.Exporter{
 		Table: func() apis.Exporter {
 			return export.NewTableExporter()
 		},
@@ -41,20 +36,24 @@ func RunCreateShiftPlan(arguments []string) error {
 		},
 	}
 
-	str := new(time.Time)
+	start := new(time.Time)
 	end := new(time.Time)
+	primaryRules := new(string)
+	secondaryRules := new(string)
 	duration := new(int)
 	teamFilePath := new(string)
-	transform := Table
+	outputFormat := Table
 	var showHelp bool
 
 	createCommand := flag.NewFlagSet("create", flag.ExitOnError)
 	createCommand.BoolVar(&showHelp, "h", false, "help for ocsctl create")
-	createCommand.IntVar(duration, "duration", 7*24, "shift duration in hours.")
-	createCommand.Func("start", "(required) start time of the schedule plan", cli.TimeValueVar(str))
+	createCommand.IntVar(duration, "duration", 7*24, "shift duration in hours")
+	createCommand.Func("start", "(required) start time of the schedule plan", cli.TimeValueVar(start))
 	createCommand.Func("end", "(required) end time of the schedule plan", cli.TimeValueVar(end))
 	createCommand.Func("team-file", "(required) path to the file that contain all on-call duties", cli.FilePathVar(teamFilePath))
-	createCommand.Func("output", "output format. One of (cvs, table, json)", cli.EnumValueVar(enums, &transform))
+	createCommand.Func("output", "output format. One of (cvs, table, json)", cli.EnumValueVar(enums, &outputFormat))
+	createCommand.StringVar(primaryRules, "primary-rules", "vacation", "Rule to decide which employee should be on-call for the next shift")
+	createCommand.StringVar(secondaryRules, "secondary-rules", "vacation", "Rule to decide which employee should be on-call for the next shift")
 	createCommand.Usage = func() {
 		fmt.Fprintf(os.Stdout, "Create on-call schedule\n")
 		fmt.Fprintf(os.Stdout, "\nUsage\n")
@@ -74,47 +73,42 @@ func RunCreateShiftPlan(arguments []string) error {
 		return nil
 	}
 
-	// check that the required flags are set...
-	if !cli.IsFlagPassed(createCommand, "start") {
+	// validate CLI arguments...
+	if ok, missed := cli.RequiredFlagPassed(createCommand, "start", "end", "team-file"); !ok {
 		createCommand.Usage()
-		return fmt.Errorf("%w: %s", ErrMissingArguments, "start")
+		return fmt.Errorf("missing required flag: %s", strings.Join(missed, ","))
 	}
 
-	if !cli.IsFlagPassed(createCommand, "end") {
-		createCommand.Usage()
-		return fmt.Errorf("%w: %s", ErrMissingArguments, "end")
-	}
-
-	if !cli.IsFlagPassed(createCommand, "team-file") {
-		createCommand.Usage()
-		return fmt.Errorf("%w: %s", ErrMissingArguments, "team-file")
-	}
-
-	// validate input data...
-	if str == end {
-		createCommand.Usage()
-		return ErrInvalidArgument
-	}
-
-	// initialize and run...
-	team, err := parse(*teamFilePath)
+	// initialize...
+	team, err := readTeamFile(*teamFilePath)
 	if err != nil {
-		return fmt.Errorf("%w: invalid team config file: %w", ErrInvalidArgument, err)
+		return fmt.Errorf("could not read %s: %w", *teamFilePath, err)
 	}
 
-	plan, err := shiftplan.NewDefaultShiftPlanner(team.Employees).Plan(*str, *end, time.Duration(*duration)*time.Hour)
+	pRules, err := mapPrimaryRules(*primaryRules)
+	if err != nil {
+		return fmt.Errorf("invalid rules: %w", err)
+	}
+
+	sRules, err := mapSecondaryRules(*secondaryRules)
+	if err != nil {
+		return fmt.Errorf("invalid rules: %w", err)
+	}
+
+	// run...
+	plan, err := shiftplan.NewShiftPlanner(team.Employees, pRules, sRules).Plan(*start, *end, time.Duration(*duration)*time.Hour)
 	if err != nil {
 		return fmt.Errorf("can not create on-call schedule: %w", err)
 	}
 
-	if err := converters[transform]().Write(plan, os.Stdout); err != nil {
+	if err := exporters[outputFormat]().Write(plan, os.Stdout); err != nil {
 		return fmt.Errorf("unexpecting error: %w", err)
 	}
 
 	return nil
 }
 
-func parse(path string) (apis.Team, error) {
+func readTeamFile(path string) (apis.Team, error) {
 	content, err := os.ReadFile(path)
 	if err != nil {
 		return apis.Team{}, fmt.Errorf("could not read file '%s': %w", path, err)
@@ -122,8 +116,50 @@ func parse(path string) (apis.Team, error) {
 
 	var team apis.Team
 	if err := json.Unmarshal(content, &team); err != nil {
-		return apis.Team{}, fmt.Errorf("could not pars json file '%s': %w", path, err)
+		return apis.Team{}, fmt.Errorf("could not parse json file '%s': %w", path, err)
 	}
 
 	return team, nil
+}
+
+func mapPrimaryRules(value string) ([]apis.Rule, error) {
+	var rules []apis.Rule
+	for _, s := range strings.Split(value, ",") {
+		switch strings.ToLower(s) {
+		case "vacation":
+			rules = append(rules, shiftplan.NewNoVacationOverlap())
+		case "minimumoneshiftgap":
+			rules = append(rules, shiftplan.NewMinimumPrimaryGapBetweenShifts(1))
+		case "minimumtwoshiftgap":
+			rules = append(rules, shiftplan.NewMinimumPrimaryGapBetweenShifts(2))
+		case "minimumthreeshiftgap":
+			rules = append(rules, shiftplan.NewMinimumPrimaryGapBetweenShifts(3))
+		case "minimumfourshiftgap":
+			rules = append(rules, shiftplan.NewMinimumPrimaryGapBetweenShifts(4))
+		default:
+			return nil, fmt.Errorf("unknow rule: %s", s)
+		}
+	}
+	return rules, nil
+}
+
+func mapSecondaryRules(value string) ([]apis.Rule, error) {
+	var rules []apis.Rule
+	for _, s := range strings.Split(value, ",") {
+		switch strings.ToLower(s) {
+		case "vacation":
+			rules = append(rules, shiftplan.NewNoVacationOverlap())
+		case "minimumoneshiftgap":
+			rules = append(rules, shiftplan.NewMinimumSecondaryGapBetweenShifts(1))
+		case "minimumtwoshiftgap":
+			rules = append(rules, shiftplan.NewMinimumSecondaryGapBetweenShifts(2))
+		case "minimumthreeshiftgap":
+			rules = append(rules, shiftplan.NewMinimumSecondaryGapBetweenShifts(3))
+		case "minimumfourshiftgap":
+			rules = append(rules, shiftplan.NewMinimumSecondaryGapBetweenShifts(4))
+		default:
+			return nil, fmt.Errorf("unknow rule: %s", s)
+		}
+	}
+	return rules, nil
 }
